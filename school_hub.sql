bac45fe4768fc5ba98114db56b063404b30f7215
ioedeveloper <daviddisu8@gmail.com>
2018-01-30 03:40:07 +0100

    New Database

--- school_hub.sql
+++ school_hub.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jan 28, 2018 at 10:03 PM
+-- Generation Time: Jan 30, 2018 at 03:37 AM
 -- Server version: 10.1.24-MariaDB
 -- PHP Version: 7.1.6
 
@@ -30,7 +30,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `tblcategory` (
   `id` int(11) NOT NULL,
-  `category_name` varchar(255) NOT NULL,
+  `Category_Name` varchar(255) NOT NULL,
   `Date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -38,59 +38,60 @@ CREATE TABLE `tblcategory` (
 -- Dumping data for table `tblcategory`
 --
 
-INSERT INTO `tblcategory` (`id`, `category_name`, `Date`) VALUES
-(1, 'University', '2018-01-27 05:36:48'),
-(2, 'Polytechnic', '2018-01-27 05:36:48');
+INSERT INTO `tblcategory` (`id`, `Category_Name`, `Date`) VALUES
+(3, 'Primary', '2018-01-29 17:02:03'),
+(4, 'Secondary', '2018-01-29 17:02:18'),
+(5, 'Tertiary', '2018-01-29 17:02:18');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tblreport`
+-- Table structure for table `tblreviews`
 --
 
-CREATE TABLE `tblreport` (
+CREATE TABLE `tblreviews` (
   `id` int(11) NOT NULL,
-  `school_id` int(11) NOT NULL,
-  `reviews` int(11) NOT NULL,
-  `reports` varchar(500) NOT NULL,
-  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  `School_Id` int(11) NOT NULL,
+  `Comment` varchar(500) NOT NULL,
+  `Rating` int(11) NOT NULL,
+  `Type` int(11) NOT NULL,
+  `Date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `tblreport`
---
-
-INSERT INTO `tblreport` (`id`, `school_id`, `reviews`, `reports`, `date`) VALUES
-(1, 1, 0, 'a very good school', '2018-01-27 05:52:16'),
-(2, 2, 0, 'a very bad school', '2018-01-27 05:54:55'),
-(3, 1, 0, 'i love the school', '2018-01-27 05:55:05'),
-(4, 2, 5, 'hjwjwjwiwiwi', '2018-01-27 08:20:47');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tblschool`
+-- Table structure for table `tblreviewtype`
 --
 
-CREATE TABLE `tblschool` (
+CREATE TABLE `tblreviewtype` (
   `id` int(11) NOT NULL,
-  `category_id` int(11) NOT NULL,
-  `school_name` varchar(255) NOT NULL,
-  `school_description` varchar(500) NOT NULL,
-  `opening_hours` varchar(250) NOT NULL,
-  `closing_hours` varchar(255) NOT NULL,
-  `address` varchar(255) NOT NULL,
-  `Date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  `Name` varchar(255) NOT NULL,
+  `Date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data for table `tblschool`
+-- Table structure for table `tblschools`
 --
 
-INSERT INTO `tblschool` (`id`, `category_id`, `school_name`, `school_description`, `opening_hours`, `closing_hours`, `address`, `Date`) VALUES
-(1, 1, 'UNN', 'School for lions', 'monday-friday', '', 'nsukka, Nigeria', '2018-01-28 20:09:28'),
-(2, 1, 'UniIbadan', 'school for kids intelligent', 'monday-friday', '', 'ibadan', '2018-01-27 05:55:27'),
-(3, 1, 'French School', 'A Short Description', '8 :00 AM', '4 :00 PM', 'Jos', '2018-01-28 20:26:52');
+CREATE TABLE `tblschools` (
+  `id` int(11) NOT NULL,
+  `Category_Id` int(11) NOT NULL,
+  `Name` varchar(255) NOT NULL,
+  `Description` varchar(500) NOT NULL,
+  `Opening_Time` varchar(250) NOT NULL,
+  `Closing_Time` varchar(255) NOT NULL,
+  `Website` varchar(255) NOT NULL,
+  `CAC` varchar(255) NOT NULL,
+  `Email` varchar(255) NOT NULL,
+  `Country` varchar(255) NOT NULL,
+  `Image` varchar(255) NOT NULL,
+  `Phonenumber` varchar(255) NOT NULL,
+  `Status` int(11) NOT NULL,
+  `Date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Indexes for dumped tables
@@ -103,18 +104,25 @@ ALTER TABLE `tblcategory`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `tblreport`
+-- Indexes for table `tblreviews`
 --
-ALTER TABLE `tblreport`
+ALTER TABLE `tblreviews`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `school_id` (`school_id`);
+  ADD KEY `school_id` (`School_Id`),
+  ADD KEY `Type` (`Type`);
 
 --
--- Indexes for table `tblschool`
+-- Indexes for table `tblreviewtype`
 --
-ALTER TABLE `tblschool`
+ALTER TABLE `tblreviewtype`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tblschools`
+--
+ALTER TABLE `tblschools`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `category_id` (`category_id`);
+  ADD KEY `category_id` (`Category_Id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -124,32 +132,38 @@ ALTER TABLE `tblschool`
 -- AUTO_INCREMENT for table `tblcategory`
 --
 ALTER TABLE `tblcategory`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
--- AUTO_INCREMENT for table `tblreport`
+-- AUTO_INCREMENT for table `tblreviews`
 --
-ALTER TABLE `tblreport`
+ALTER TABLE `tblreviews`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
--- AUTO_INCREMENT for table `tblschool`
+-- AUTO_INCREMENT for table `tblreviewtype`
 --
-ALTER TABLE `tblschool`
+ALTER TABLE `tblreviewtype`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `tblschools`
+--
+ALTER TABLE `tblschools`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `tblreport`
+-- Constraints for table `tblreviews`
 --
-ALTER TABLE `tblreport`
-  ADD CONSTRAINT `tblreport_ibfk_1` FOREIGN KEY (`school_id`) REFERENCES `tblschool` (`id`);
+ALTER TABLE `tblreviews`
+  ADD CONSTRAINT `tblreviews_ibfk_1` FOREIGN KEY (`Type`) REFERENCES `tblreviewtype` (`id`),
+  ADD CONSTRAINT `tblreviews_ibfk_2` FOREIGN KEY (`School_Id`) REFERENCES `tblschools` (`id`);
 
 --
--- Constraints for table `tblschool`
+-- Constraints for table `tblschools`
 --
-ALTER TABLE `tblschool`
-  ADD CONSTRAINT `tblschool_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `tblcategory` (`id`);
+ALTER TABLE `tblschools`
+  ADD CONSTRAINT `tblschools_ibfk_1` FOREIGN KEY (`Category_Id`) REFERENCES `tblcategory` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
